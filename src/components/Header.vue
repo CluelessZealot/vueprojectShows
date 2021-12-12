d20eaf48c4bf614c113ca75cdf1c075a67c9016f
Shane Parenteau <sp0555121@otc.edu>
2021-12-12 13:55:54 -0600

    added Header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class = "Header">
+        <h1>{{theTitle}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props:['theTitle']
+}
+</script>
+
+<style scoped>
+    .Header
+    {
+        background: #333;
+        color: white;
+        text-align: center;
+        padding: 10px;
+        margin: 0px;
+    }
+</style>
