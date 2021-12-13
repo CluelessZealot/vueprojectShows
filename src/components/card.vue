918241fcc306482a5aef4c97f3539dab4c9d41e4
Shane Parenteau <sp0555121@otc.edu>
2021-12-13 10:56:57 -0600

    check 1

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
+    <div class="card" style="width: 18rem;">
+    <img v-bind:src="'https://image.tmdb.org/t/p/w500' + movieobj.poster_path" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">{{}}</h5>
+    <p class="card-text">{{}}</p>
+    </div>
     </div>
 </template>
 
 <script>
 export default {
+    name: "Card",
+    props:[
+        'Title',
+        'text',
+        'path'
+    ]
     
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+var movieTickets = [];
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  mounted(){
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=8087d190320d397a65275f06876bcc36&language=en-US&page=1')
+        .then((response) => {
+            movies = response.data.results;
+
+            tempArray = movies;
+            // gets the array, splices it and changes its variables pushing it into the movieTickets array
+            tempArray.splice(3,17);
+            tempArray.forEach(movieTickets => {
+                var newMovie =  {
+                    title: movieTickets.title,
+                    overview: movieTickets.overview,
+                    poster_path: movieTickets.poster_path,
+                    childTickets: 0,
+                    adultTickets: 0
+                }
+                this.movieTickets.push(newMovie)
+            })
+            
+
+            console.log(this.movieTickets);
+        })
+ }
+  
 }).$mount('#app')
