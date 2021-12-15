6b93cba59fc0198bf9b16c420f7a855ade164e1c
Shane Parenteau <sp0555121@otc.edu>
2021-12-15 01:11:52 -0600

    final version

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
+<!--
+dev: Shane Parenteau
+project: tv shows
+class: CIS 131
+date: 12/15/21
+-->
+
 <template>
   <div id="app">
    
    <Header theTitle = "Popular TV Shows"/>
+   
+    <div id ="app" class = "row justify-content-md-between">
+        <card v-for = "movie in movies" v-bind:movieObj=movie v-bind:key="movie.id"/>
+        
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import axios from 'axios'
+import card from './components/card.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    card
+  },
+  data(){
+    return{
+     movies: [],
+    }
+  },
+  mounted(){
+    axios.get('https://api.themoviedb.org/3/tv/popular?api_key=8087d190320d397a65275f06876bcc36&language=en-US&page=1')
+                .then((response) => {
+                    this.movies = response.data.results;
+                    
+                    this.movies.splice(3,17);
+                    console.log(this.movies);
+                })
   }
-//   data(){
-//     return{
-
-//     }
-//   }
 }
 </script>
 
@@ -25,5 +49,8 @@ export default {
   *{
     padding: 0px;
     margin: 0px;
+  }
+  #app{
+    background: black;
   }
 </style>
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="card" style="width: 18rem;">
-    <img v-bind:src="'https://image.tmdb.org/t/p/w500' + movieobj.poster_path" class="card-img-top" alt="...">
+    <img v-bind:src="'https://image.tmdb.org/t/p/w500' + movieObj.poster_path" class="card-img-top" alt="...">
     <div class="card-body">
-    <h5 class="card-title">{{}}</h5>
-    <p class="card-text">{{}}</p>
+    <h5 class="card-title">{{movieObj.name}}</h5>
+    <p class="card-text">{{movieObj.overview}}</p>
     </div>
     </div>
 </template>
@@ -12,14 +12,17 @@
 export default {
     name: "Card",
     props:[
-        'Title',
-        'text',
-        'path'
+        'movieObj'
     ]
     
 }
 </script>
 
 <style scoped>
-
+    .card{
+        background: lightblue;
+        height: inherit;
+        margin-left: 110px;
+        margin-right: 110px;
+    }
 </style>
